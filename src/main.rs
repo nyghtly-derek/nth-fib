6c16ca10aad859b01bb2f394b202a0440297a4c2
Derek Blankenship <derekblankenship05@gmail.com>
2018-05-30 19:31:03 -0400

    documented functions

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ fn main() {
     println!("Calculating Fibonacci Sequence...");
     let mut fib_seq: HashMap<u64, u64> = HashMap::new();
     fib_seq.insert(0, 0);
-    fib_seq.insert(1, 1);
+    fib_seq.insert(1, 1); // initializing base cases
     let term_number = get_term_number();
     let answer = recursive_fib(term_number, &mut fib_seq);
     println!("The value for term {} of the fibonacci sequence is: {}", term_number, answer);
 }
 
+
+/// uses recursion and memoization to return the nth term of the fib sequence
 fn recursive_fib(n: u64, fib_seq: &mut HashMap<u64, u64>) -> u64 {
     {
         let nth_fib = fib_seq.get(&n);
@@ -24,6 +26,7 @@ fn recursive_fib(n: u64, fib_seq: &mut HashMap<u64, u64>) -> u64 {
     return nth_fib
 }
 
+/// returns a line from the user
 fn get_line() -> String {
     let mut line = String::new();
 
@@ -33,6 +36,7 @@ fn get_line() -> String {
     return line
 }
 
+/// asks for a number from the user repeatedly until a number is given
 fn get_term_number() -> u64 {
      loop {
         println!("Enter term number...");
